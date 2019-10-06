96e7f100ce9774872c4a3067fd6b54afb5c6ee12
Jani Leppäkoski <jani.leppakoski@heeros.com>
2019-10-06 20:51:51 +0300

    members CRUD

--- controllers/members.js
+++ controllers/members.js
@@ -25,4 +25,44 @@ membersRouter.get('/', async (request, response) => {
   response.json(members.map(member => member.toJSON()))
 })
 
+membersRouter.get('/:id', async (request, response, next) => {
+  try {
+    const member = await Member.findById(request.params.id)
+    console.log(member)
+    if (member) {
+      response.json(member.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+membersRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Member.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+membersRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const member = {
+    firstName: body.firstName,
+    lastName: body.lastName,
+    favoriteAuthor: body.favoriteAuthor,
+    motto: body.motto
+  }
+
+  Member.findByIdAndUpdate(request.params.id, member, { new: true })
+    .then(updatedMember => {
+      response.json(updatedMember.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 module.exports = membersRouter
