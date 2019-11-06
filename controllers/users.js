e6f1e9a9bb636fcc0323c3e68c19933d37468533
Jani Leppäkoski <jani.leppakoski@heeros.com>
2019-11-06 18:36:19 +0200

    fixing and hooning

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 usersRouter.get('/', async (request, response) => {
   const users = await User
     .find({})
-  response.json(users.map(User.format))
+  response.json(users.map(user => user.toJSON()))
 })
 
 usersRouter.post('/', async (request, response) => {
